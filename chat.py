1b8cf6896bc1e11c9cda03462ce3508cfd64108a
Forestriver <vitaly.romas1@gmail.com>
2019-10-15 20:05:51 +0300

    Join user update

--- chat.py
+++ chat.py
@@ -1,7 +1,8 @@
 from os import environ
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send
+from flask import session
 
 load_dotenv(find_dotenv())
 app = Flask(__name__)
@@ -9,18 +10,20 @@ app.config['SECRET_KEY'] = environ.get('KEY')
 socketio = SocketIO(app)
 
 @app.route('/')
-def sessions():
+def index():
     return render_template('session.html')
 
-def messageReceived(methods=['GET', 'POST']):
-    print("Message delivered.")
+@socketio.on('message')
+def handle_message(msg):
+    print(msg)
+    send(msg, broadcast = True)
 
-@socketio.on('connection')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print("My event received: " + str(json))
-    socketio.emit('message', json, callback=messageReceived)
+@socketio.on('username')
+def join_username(username):
+    if username != '':
+        socketio.username = username
+        socketio.emit('user' + 'has joined', username)
 
 
-
-if __name__ == "__main__":
-    socketio.run(app, debug = True)
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
