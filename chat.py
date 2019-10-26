4773fadf6ca339470bfb33c6dc7bd2d106923e1d
Forestriver <vitaly.romas1@gmail.com>
2019-10-26 12:23:54 +0300

    backend update

--- chat.py
+++ chat.py
@@ -1,26 +1,30 @@
+import os
 from os import environ
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, send
 from flask import session
 
+
 load_dotenv(find_dotenv())
 app = Flask(__name__)
+#Creating path to the file with secret key
 app.config['SECRET_KEY'] = environ.get('KEY')
-socketio = SocketIO(app)
+socketio = SocketIO(app, manage_session = False)
+
 
 @app.route('/')
 def index():
     return render_template('session.html')
 
+
 @socketio.on('message')
 def handle_message(msg):
-    print(msg)
     send(msg, broadcast = True)
 
 @socketio.on('join')
 def join_username(username):
-    emit('message', {'user':'Server', 'message':username  + ' joined the chat'}, broadcast = True)
+    emit('message', {'user':'Chat', 'message':username  + ' joined this chatroom'}, broadcast = True)
 
 
 if __name__ == '__main__':
